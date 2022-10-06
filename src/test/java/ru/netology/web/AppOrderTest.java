ca9fc0d953568d2936400d194d40df0ddd9ec1c7
Maria Mikhno <mmikhno89@gmail.com>
2022-10-06 19:30:22 +0300

    Happy path

---
+++ src/test/java/ru/netology/web/AppOrderTest.java
@@ -0,0 +1,46 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class AppOrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void SetupAll() {
+        System.setProperty("webdriver.chrome.driver", "./drivers/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        // driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void shouldTestFormPositive() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Анна-Мария Иванова");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79211234567");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+}
